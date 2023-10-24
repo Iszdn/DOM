0e8b3d622d9a7b89f2fa3cef78dea849ecf8583d
Orxan-Abdullazada <tu7pnc7lt@code.edu.az>
2023-10-24 13:39:46 +0400

    DOM

---
+++ assets/main.js
@@ -0,0 +1,53 @@
+
+// let div=document.createElement('div')
+// let btn=document.createElement('button')
+// let body=document.querySelector('body')
+// div.style.width="300px"
+//  div.style.height="300px"
+
+// div.style.backgroundColor="pink"
+// btn.style.width="50px"
+// btn.style.height="20px"
+// btn.style.marginTop="20px"
+// body.append(div)
+// body.append(btn)
+// btn.onclick=()=>{
+//     div.style.backgroundColor=""
+// }
+// btn.onclick=()=>{
+//     div.style.backgroundColor="blue"
+// }
+// btn.onclick=()=>{
+//     div.style.backgroundColor="yellow"
+// }
+// btn.onclick=()=>{
+//     div.style.backgroundColor="violet"
+//     div.style.width="200px"
+//  div.style.height="200px"
+//  btn.style.height="10px"
+//  btn.style.width="30px"
+// }
+// 111111
+// let elem=document.getElementById('elem')
+// console.log(elem);
+// 22222222222
+// let button=document.querySelector('button')
+// button.onclick=()=>{
+//     alert("Alert")
+// }
+// 33333333333
+// let div1=document.querySelector('.div1')
+// div1.onclick=()=>{div1.style.backgroundColor="violet"}
+// 44444444444444444
+// let div2=document.querySelector('.div2')
+// div2.onclick=()=>{div2.classList.add('active');}
+// 555555555555555555
+// let div2=document.querySelector('.div2')
+// div2.onclick=()=>{div2.classList.toggle('active');}
+// 66666666666
+// let h2=document.querySelector('h2')
+// alert(h2.textContent)
+// 777777777
+
+// let btn=document.querySelectorAll('.btn')
+// 8888888888888888
